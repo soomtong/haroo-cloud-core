d8804e956b56949ef01c126f24e5cf16da8f6423
soomtong <soomtong@gmail.com>
2015-01-09 05:28:07 +0900

    `init` scaffold with logger

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 require('./lib/setup')();
 
-//todo: bind global exception
-
 var app = {
     node_env: process.env.NODE_ENV || 'development',
     init: require('./lib/route'),
@@ -9,11 +7,27 @@ var app = {
 };
 
 app.init(app.node_env, function (server) {
+    var restify = require('restify');
+    var bunyan = require('bunyan');
+
     var config = app.config({mode: app.node_env});
 
     server.listen(config.server.port, function serverStarted() {
         //console.log('%s listening at %s', server.name, server.url);
     });
+
+    server.on('after', restify.auditLogger({
+        log: bunyan.createLogger({
+            name: 'haroo-api',
+            streams: [
+                {level: 'info', path: 'log/info.log'},
+                {level: 'error', path: 'log/error.log'}
+            ]
+        })
+    }));
+
+    //todo: bind global exception
+
 });
 
 module.exports = app;
