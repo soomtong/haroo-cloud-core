bde87082c48246f2f3856be5ac855c37cde826fe
soomtong <soomtong@gmail.com>
2015-04-23 13:35:22 +0900

    `fix` 401

--- core/api/account.js
+++ core/api/account.js
@@ -152,7 +152,7 @@ exports.readAccount = function (req, res, next) {
     Account.findOne({email: params.email}, function (err, existUser) {
         if (!existUser) {
             msg = i18n.t('account.read.mismatch');
-            result = feedback.badRequest(msg, params);
+            result = feedback.unauthorized(msg, params);
 
             return res.json(result.statusCode, result);
         }
