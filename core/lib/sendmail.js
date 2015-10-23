c8687d6a805163fbb2a7cc1dc45c999b9c79b677
soomtong <soomtong@gmail.com>
2015-10-23 21:31:25 +0900

    `updated` remove mail template and some more

--- core/lib/sendmail.js
+++ core/lib/sendmail.js
@@ -1,30 +1,40 @@
 var nodemailer = require('nodemailer');
-var emailTemplates = require('swig-email-templates');
 
 exports.sendPasswordResetMailByDelegate = function (address, context, emailToken, callback) {
-    var smtpTransport = nodemailer.createTransport(emailToken);
+    var transporter = nodemailer.createTransport(emailToken);
 
-    emailTemplates({root: __dirname + "/templates"}, function (error, render) {
-        var email = {
-            from: emailToken['reply'], // sender address
-            to: address,
-//            bcc: emailToken.bcc,
-            subject: "Reset your password link described from haroo-cloud"
-        };
+    var email = {
+        from: emailToken['reply'], // sender address
+        to: address,
+        //bcc: emailToken.bcc,
+        subject: "Reset your password link described from haroo-cloud",
+        html: "<h1>Haroo Cloud</h1><h2>Hi, Change your password from here link</h2><p><a href="
+        + context.link + " target=\"_blank\">Reset Your Password for Haroonote</a></p>"
+    };
 
-        render('password_reset_email.html', context, function (error, html) {
-            email.html = html;
+    transporter.sendMail(email, function (error, info) {
+        if (error) {
+            callback && callback(error);
+        } else {
+            callback && callback(info);
+        }
 
-            smtpTransport.sendMail(email, function (error, info) {
-                if (error) {
-                    callback && callback(error);
-                } else {
-                    callback && callback(info);
-                }
-
-                // if you don't want to use this transport object anymore, uncomment following line
-                smtpTransport.close(); // shut down the connection pool, no more messages
-            });
-        });
+        transporter.close();
     });
+
+
+    /*
+     template({root: __dirname + "/templates"}, function (error, render) {
+     var email = {
+     from: emailToken['reply'], // sender address
+     to: address,
+     //            bcc: emailToken.bcc,
+     subject: "Reset your password link described from haroo-cloud"
+     };
+
+     render('password_reset_email.html', context, function (error, html) {
+     email.html = html;
+
+     });
+     });*/
 };
