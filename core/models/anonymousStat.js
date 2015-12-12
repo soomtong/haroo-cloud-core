7b1046f128ad32956e2497b57cfc299e2fdefc3f
soomtong <soomtong@gmail.com>
2015-12-12 21:51:39 +0900

    `fixed` field name

--- core/models/anonymousStat.js
+++ core/models/anonymousStat.js
@@ -5,7 +5,7 @@ var publicDocSchema = new mongoose.Schema({
     created_at: Date,
     view_count: Number,
     commend_count: Number,
-    alert_count: Number
+    claim_count: Number
 });
 
 module.exports = mongoose.model('anonymous_stat', publicDocSchema);
